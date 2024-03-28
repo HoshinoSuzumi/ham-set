7f470553296df5ab128752d093bd1230d59811db
HoshinoSuzumi <master@uniiem.com>
2024-03-28 14:55:40 +0800

    wip: og test

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -1,8 +1,13 @@
 import {ImageResponse} from 'next/og';
+import {Icon} from "@iconify-icon/react";
 
 export const runtime = 'edge';
+export const contentType = 'image/png';
 
 export async function GET() {
+  const fontData = await fetch(
+    new URL('../../../assets/font/NotoSansSC-VF.ttf', import.meta.url),
+  ).then((res) => res.arrayBuffer());
   return new ImageResponse(
     (
       <div
@@ -13,17 +18,26 @@ export async function GET() {
           width: '100%',
           height: '100%',
           padding: '50px 200px',
+          display: 'flex',
           textAlign: 'center',
           justifyContent: 'center',
           alignItems: 'center',
         }}
       >
-        👋 Hello
+        <Icon icon={'tabler:antenna'} style={{fontSize: 100}}/>
+        👋 Hello 你好
       </div>
     ),
     {
       width: 1200,
       height: 630,
+      fonts: [
+        {
+          name: 'Noto Sans SC',
+          data: fontData,
+          style: 'normal',
+        }
+      ]
     },
   );
 }
