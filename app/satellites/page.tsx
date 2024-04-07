8bfcd7d8c3ea91a1a5e617d5e0d5c7d3a65c1f3c
HoshinoSuzumi <master@uniiem.com>
2024-04-08 01:23:49 +0800

    chore: example

--- app/satellites/page.tsx
+++ app/satellites/page.tsx
@@ -1,13 +1,70 @@
 'use client'
 
+import './styles.scss'
 import {Icon} from '@iconify-icon/react'
 import {noto_sc, rubik} from '@/app/fonts'
-import {Input} from '@douyinfe/semi-ui'
+import {Input, Table} from '@douyinfe/semi-ui'
 import {IconSearch} from '@douyinfe/semi-icons'
+import useSWR from 'swr'
+import {ColumnProps} from '@douyinfe/semi-ui/lib/es/table'
 
 export default function Page() {
+  const columns: ColumnProps<{
+    status: string
+    name: string
+    mode: string
+  }>[] = [
+    {
+      title: '状态',
+      dataIndex: 'status',
+    },
+    {
+      title: '卫星名称',
+      dataIndex: 'name',
+    },
+    {
+      title: 'NORAD ID',
+      dataIndex: 'norad_id',
+    },
+    {
+      title: '上行',
+      dataIndex: 'uplink',
+    },
+    {
+      title: '下行',
+      dataIndex: 'downlink',
+    },
+    {
+      title: '信标',
+      dataIndex: 'beacon',
+    },
+    {
+      title: '模式',
+      dataIndex: 'mode',
+      filters: [
+        {
+          text: 'FM',
+          value: 'FM',
+        },
+        {
+          text: 'VHF/UHF',
+          value: 'VHF/UHF',
+        },
+      ],
+      onFilter: (value, record) => {
+        console.log(value, record?.mode)
+        return record?.mode?.includes(value) || false
+      },
+    },
+  ]
+
+  const {
+    data,
+    isLoading,
+  } = useSWR('https://mirror.ghproxy.com/https://raw.githubusercontent.com/palewire/ham-satellite-database/main/data/amsat-all-frequencies.json')
+
   return (
-    <div className={'w-full h-full flex flex-col items-center p-8 bg-white dark:bg-neutral-900'}>
+    <div className={'w-full h-full flex flex-col gap-8 items-center pt-8 md:p-8 bg-white dark:bg-neutral-900'}>
       <div>
         <h1 className={`flex flex-col items-center text-lg font-medium ${noto_sc.className}`}>
           <Icon icon={'tabler:satellite'} className={'text-4xl mb-2'}/>
@@ -19,7 +76,13 @@ export default function Page() {
           className={'!w-64 mt-4'}
           size={'large'}
           prefix={<IconSearch/>}
-          clearIcon
+        />
+      </div>
+      <div className={'flex-1 w-full md:w-[80%] md:border md:rounded-lg md:overflow-hidden'}>
+        <Table
+          columns={columns}
+          dataSource={data}
+          loading={isLoading}
         />
       </div>
     </div>
