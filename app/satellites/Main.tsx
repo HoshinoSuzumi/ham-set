1ee14872102fe754a3e15f4686a1a60abdc470ed
HoshinoSuzumi <master@uniiem.com>
2024-04-13 03:47:49 +0800

    fix: hide qr on mobile

--- app/satellites/Main.tsx
+++ app/satellites/Main.tsx
@@ -164,18 +164,22 @@ export const Main = () => {
                   />
                 </Tooltip>
               ) }
-              <Tooltip content={ '手机扫码获取位置' } position={ 'bottom' }>
-                <Button
-                  icon={ <svg xmlns="http://www.w3.org/2000/svg" width="1.2em" height="1.2em" viewBox="0 0 24 24">
-                    <path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                          d="M4 5a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v4a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1zm3 12v.01M14 5a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v4a1 1 0 0 1-1 1h-4a1 1 0 0 1-1-1zM7 7v.01M4 15a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v4a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1zm13-8v.01M14 14h3m3 0v.01M14 14v3m0 3h3m0-3h3m0 0v3"/>
-                  </svg> }
-                  theme={ 'borderless' }
-                  type={ 'tertiary' }
-                  size={ 'small' }
-                  onClick={ () => setMobileLocationRefId(uuidv4()) }
-                />
-              </Tooltip>
+              { !locationFromMobile && (
+                <Tooltip content={ '手机扫码获取位置' } position={ 'bottom' }>
+                  <Button
+                    icon={ <svg xmlns="http://www.w3.org/2000/svg" width="1.2em" height="1.2em" viewBox="0 0 24 24">
+                      <path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M4 5a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v4a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1zm3 12v.01M14 5a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v4a1 1 0 0 1-1 1h-4a1 1 0 0 1-1-1zM7 7v.01M4 15a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v4a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1zm13-8v.01M14 14h3m3 0v.01M14 14v3m0 3h3m0-3h3m0 0v3"/>
+                    </svg> }
+                    className={ '!hidden md:!block' }
+                    theme={ 'borderless' }
+                    type={ 'tertiary' }
+                    size={ 'small' }
+                    onClick={ () => setMobileLocationRefId(uuidv4()) }
+                  />
+                </Tooltip>
+              ) }
             </div>
           </div>
         </div>
