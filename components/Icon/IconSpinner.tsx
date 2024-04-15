47f5c098486c93142ad8fb40e165ff0b62f2467a
HoshinoSuzumi <master@uniiem.com>
2024-04-15 15:39:27 +0800

    fix: loading spinner does not work on ios safari

--- components/Icon/IconSpinner.tsx
+++ components/Icon/IconSpinner.tsx
@@ -1,14 +1,21 @@
-import {SVGProps} from 'react';
+import styles from './styles.module.scss'
+import { SVGProps } from 'react'
 
 export function IconSpinner(props: SVGProps<SVGSVGElement>) {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}>
+    <svg xmlns="http://www.w3.org/2000/svg"
+         width="1em"
+         height="1em"
+         viewBox="0 0 24 24"
+         { ...{
+           ...props,
+           className: `${ styles.spinner } ${ props.className }`,
+         } }
+    >
       <path fill="currentColor" d="M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,19a8,8,0,1,1,8-8A8,8,0,0,1,12,20Z"
             opacity=".25"></path>
       <path fill="currentColor"
             d="M10.14,1.16a11,11,0,0,0-9,8.92A1.59,1.59,0,0,0,2.46,12,1.52,1.52,0,0,0,4.11,10.7a8,8,0,0,1,6.66-6.61A1.42,1.42,0,0,0,12,2.69h0A1.57,1.57,0,0,0,10.14,1.16Z">
-        <animateTransform attributeName="transform" dur="0.75s" repeatCount="indefinite" type="rotate"
-                          values="0 12 12;360 12 12"></animateTransform>
       </path>
     </svg>
   )
